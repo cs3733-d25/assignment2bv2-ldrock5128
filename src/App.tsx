35d2593cc38bb53c56d9b9f1cf7316ff769d598c
himynameismaggie <mghosie@wpi.edu>
2025-04-03 16:29:47 -0400

    Maggie Assign2B sub

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import './App.css'
 import Title from './components/title';
 import Intro from './components/intro';
 import Hobby1 from "./components/hobby1";
+import Hobby2 from "./components/hobby2";
 
 const App =() => {
   return (
       <div>
           <Title/>
-          <Intro name1 = "Lucien" name2 = "Maggie" />
-          <br />
-          <Hobby1 />
+          <Intro name1="Lucien" name2="Maggie"/>
+          <br/>
+          <Hobby1/>
+          <br/>
+          <Hobby2/>
       </div>
   )
 }
---
+++ src/components/table2.tsx
@@ -0,0 +1,40 @@
+const Table2= () => {
+    return (
+        <table className="collapse">
+            <tbody>
+            <tr>
+                <th>Driver</th>
+                <th>WDC Points</th>
+                <th>Fantasy Points</th>
+                <th>Thoughts</th>
+            </tr>
+            <tr>
+                <td>Max Verstappen</td>
+                <td>36</td>
+                <td>59</td>
+                <td>Neutral</td>
+            </tr>
+            <tr>
+                <td>Yuki Tsunoda</td>
+                <td>3</td>
+                <td>6</td>
+                <td>Disappointed with the team</td>
+            </tr>
+            <tr>
+                <td>Isack Hadjar</td>
+                <td>0</td>
+                <td>-9</td>
+                <td>Mixed</td>
+            </tr>
+            <tr>
+                <td>Fernando Alonso</td>
+                <td>0</td>
+                <td>-36</td>
+                <td>Depression</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table2;
